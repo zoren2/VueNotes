f1d1828a19e585b45855e32df688824d0f098046
Jack Tsang <cic_tian2@yahoo.com>
2019-07-14 21:18:00 -0700

    Added initial boiler plate code for inputComponent template

--- app/main.js
+++ app/main.js
@@ -1,9 +1,24 @@
 const inputComponent = {
-    template: `<input class="input is-small" type="text" />`
+    template: `<input
+        :placeholder="placeholder"
+        v-model="input"
+        @keyup.enter="monitorEnterKey"
+        class="input is-small" type="text" />`,
+    props: ['placeholder'],
+    data() {
+        return {
+            input: ''
+        }
+    }
 }
 
 new Vue({
     el: '#app',
+    data: {
+        notes: [],
+        timestamps: [],
+        placeholder: 'Enter a note'
+    },
     components: {
         'input-component': inputComponent
     }
